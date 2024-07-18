d3fb673e6c6f210b2567a9dffed06995d0d0a4c7
Collin <collin@itbsoftware.co.za>
2024-07-18 20:13:48 +0200

    Make scene and script stay when opening files

--- addons/script-dock/plugin.gd
+++ addons/script-dock/plugin.gd
@@ -3,11 +3,11 @@ extends EditorPlugin
 
 var control := MarginContainer.new()
 var panel: Control
-var default_parent: Control 
-
-
+var default_parent: Control  
 var script_editor :ScriptEditor = null
-func _enter_tree():
+var last_screen:=''
+
+func _enter_tree()->void:
 	script_editor = EditorInterface.get_script_editor()
 	script_editor.editor_script_changed.connect(script_visibility_changed)
 	script_editor.visibility_changed.connect(script_visibility_changed)
@@ -16,22 +16,36 @@ func _enter_tree():
 	panel = default_parent.get_child(0)
 	panel.reparent(control)
 	
-	
 	control.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	control.size_flags_vertical = Control.SIZE_EXPAND_FILL
 	control.name = "Scripts"
 	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_BR, control)
+	## connect to what screen is active 
+	main_screen_changed.connect(screen_changed)
 	
-	var parent: TabContainer = control.get_parent()
-	parent.print_tree()
-	
-	
- 
-func script_visibility_changed(a = null):
-	if !script_editor.is_visible_in_tree():
+func screen_changed(screen)->void:
+	last_screen = screen
+	if !script_editor && !script_editor.is_visible_in_tree():
 		return
 	var parent: TabContainer = control.get_parent()
-	parent.current_tab = control.get_index()
+	
+	match screen:
+		"Script":
+			parent.current_tab = control.get_index()
+		_:
+			parent.current_tab = 0
+			
+ 
+func script_visibility_changed(a = null)->void:
+	if !script_editor && !script_editor.is_visible_in_tree():
+		return
+	
+	if last_screen=="":
+		last_screen="Script"
+		
+	if last_screen=="Script":
+		var parent: TabContainer = control.get_parent()
+		parent.current_tab = control.get_index()
 
 func _exit_tree():
 	panel.reparent(default_parent)
