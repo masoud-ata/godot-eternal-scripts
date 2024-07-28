cb1b8e675ed33c620d4fed0a3006743e727391ce
Mohammad Attari <masoud.ata@gmail.com>
2024-07-28 20:49:32 +0200

    make sure the list visibility is handled properly if Ctrl+Shift+F11 is pressed

--- addons/eternal-scripts/plugin.gd
+++ addons/eternal-scripts/plugin.gd
@@ -10,6 +10,9 @@ var new_parent: Control
 var new_scripts_dock := MarginContainer.new()
 
 
+var docked := false
+
+
 func _enter_tree():
 	print("Eternal-scripts is on baby!")
 	script_editor = get_editor_interface().get_script_editor()
@@ -20,6 +23,7 @@ func _enter_tree():
 	scripts_panel = original_parent.get_child(0)
 
 	_add_scripts_panel_to_new_scripts_dock()
+	docked = true
 
 
 func _exit_tree():
@@ -29,6 +33,19 @@ func _exit_tree():
 	script_editor.visibility_changed.disconnect(_on_editor_script_changed)
 
 	_revert_to_original()
+	new_scripts_dock.queue_free()
+	docked = false
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.is_pressed() and not event.is_echo():
+		var key_event = event as InputEventKey
+		if key_event.shift_pressed and key_event.ctrl_pressed and key_event.keycode == KEY_F11:
+			if docked:
+				_revert_to_original()
+			else:
+				_add_scripts_panel_to_new_scripts_dock()
+			docked = not docked
 
 
 func _add_scripts_panel_to_new_scripts_dock() -> void:
@@ -42,15 +59,14 @@ func _add_scripts_panel_to_new_scripts_dock() -> void:
 	new_parent = new_scripts_dock.get_parent()
 
 
+func _revert_to_original() -> void:
+	scripts_panel.reparent(original_parent)
+	original_parent.move_child(scripts_panel, 0)
+	remove_control_from_docks(new_scripts_dock)
+
+
 func _on_editor_script_changed(_a = null):
 	get_editor_interface().set_main_screen_editor("Script")
 	if !script_editor.is_visible_in_tree():
 		return
 	new_parent.current_tab = new_scripts_dock.get_index()
-
-
-func _revert_to_original() -> void:
-	scripts_panel.reparent(original_parent)
-	original_parent.move_child(scripts_panel, 0)
-	remove_control_from_docks(new_scripts_dock)
-	new_scripts_dock.queue_free()
